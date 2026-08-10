893f2fae5b8487e1285afffba75e329fda6aee75
ahmed <kenan.begic.2002@gmail.com>
2026-08-10 23:08:57 +0200

    refactor: add page and layout

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,33 @@
+// app/(admin)/layout.tsx
+"use client";
+
+import React from "react";
+import { useSidebar } from "../context/SidebarProvider";
+import AppSidebar from "../layout/AppSidebar";
+import AppHeader from "../layout/AppHeader";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isOpen, isHovered } = useSidebar();
+  
+  const toggleSidebar = 
+    isOpen || isHovered 
+      ? "lg:ml-72" 
+      : "lg:ml-22.5"
+      
+  return (
+    <div className="flex min-h-screen w-full">
+      <AppSidebar />
+      <div
+        className={`flex min-w-0 flex-1 flex-col transition-all duration-300 ease-in-out
+          ${toggleSidebar}`}
+      >
+        <AppHeader />
+        <main className="min-w-0 mt-16 flex-1">{children}</main>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(admin)/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p className="mt-2 text-gray-600">This is my dashboard.</p>
+    </div>
+  );
+}
