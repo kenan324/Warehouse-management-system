9da2fbec38884b12f4911e9371811bf3a7c9380b
ahmed <kenan.begic.2002@gmail.com>
2026-08-12 17:06:56 +0200

    feat: add component card

---
+++ src/app/components/componentsCards/mainComponentCard/ComponentCard.tsx
@@ -0,0 +1,29 @@
+
+interface ComponentsCardProps {
+    title: string,
+    children : React.ReactNode,
+    className?: string
+}
+
+const ComponentsCard: React.FC<ComponentsCardProps> = ({
+    title,
+    children,
+    className = ""
+}) => {
+    return (
+        <div className={`rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/3
+        ${className}`}
+        >
+            <div className="px-6 py-5">
+                <h3 className="text-base font-bold text-gray-800 dark:text-white/90">
+                    {title}
+                </h3>
+            </div>
+            <div className="p-4 border-t border-gray-100 dark:border-gray-800 sm:p-6">
+                <div className="space-y-6">{children}</div>
+            </div>
+        </div>
+    )
+};
+
+export default ComponentsCard;
