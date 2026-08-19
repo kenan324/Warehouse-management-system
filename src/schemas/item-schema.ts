da630282313a4622fe5849085fc06fab656725c9
ahmed <kenan.begic.2002@gmail.com>
2026-08-19 16:58:07 +0200

    feat: add action form to item schema
    Add Field error type for validating server action
    and returning a response to form.

--- src/schemas/item-schema.ts
+++ src/schemas/item-schema.ts
@@ -15,3 +15,17 @@ export const updateItemSchema = createItemSchema.partial();
 export type ItemSchemaType = z.Infer<typeof itemSchema>;
 export type CreateItemSchemaType = z.Infer<typeof createItemSchema>;
 export type UpdateItemSchemaType = z.Infer<typeof updateItemSchema>;
+
+type FieldErrors = Partial<Record<keyof CreateItemSchemaType, string>>;
+
+export type ActionResult = {
+    success: boolean,
+    error?: string,
+    fieldErrors?: FieldErrors
+}
+
+export const initialActionState: ActionResult = {
+    success: false,
+    error: "",
+    fieldErrors: {},
+}
