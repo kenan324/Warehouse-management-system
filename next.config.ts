3787be038801a5da7f73c4439f415ba09c4b00f0
ahmed <kenan.begic.2002@gmail.com>
2026-08-21 23:11:40 +0200

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = { 
-    turbopack: {
-      rules: {
-        '*.svg': {
-          loaders: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgo: true,
-              svgoConfig: {
-                plugins: [
-                  {
-                    name: "preset-default",
-                    params: {
-                      overrides: {
-                        removeViewBox: false,
-                      },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com"
+      },
+    ],
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
                     },
                   },
-                  "removeAttrs",
-                ],
-              },
-              svgProps: {
-                fill: "currentColor",
-              },
+                },
+                "removeAttrs",
+              ],
+            },
+            svgProps: {
+              fill: "currentColor",
             },
           },
-          ],
-          as: '*.js',
         },
+        ],
+        as: '*.js',
       },
     },
+  },
 };
 
 export default nextConfig;
