221d032a9a2650f03ee5959e09e7e3390967d8b7
ahmed <kenan.begic.2002@gmail.com>
2026-08-21 17:38:06 +0200

    fix: return the array of initialized database app

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 
-import { getApp, initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
@@ -16,6 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = getApp().length ? getApp() : initializeApp(firebaseConfig);
+export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
