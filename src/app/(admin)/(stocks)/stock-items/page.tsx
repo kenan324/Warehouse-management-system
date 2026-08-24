b9e2aec94fe3154afa0ec013dbae33b0982cf3b6
ahmed <kenan.begic.2002@gmail.com>
2026-08-24 15:22:38 +0200

    chore(css): adjust height and flex to the main component
    Moved itemsExist to the search bar div

--- src/app/(admin)/(stocks)/stock-items/page.tsx
+++ src/app/(admin)/(stocks)/stock-items/page.tsx
@@ -30,24 +30,25 @@ export default function Stock() {
     }, [])
     return (
         
-        <div className="flex-1 p-6 space-y-2">
+        <div className="flex-1 flex flex-col p-6 space-y-2 min-h-full">
             <PageBreadcrumbNav pageTitle="Inventory" path="inventory/stock-items" />
-            <div className="flex items-center justify-between mt-4 p-6 bg-white rounded-lg shadow-md">
-                <Searchbar />
-                <Button size="sm" variant="primary" 
-                className={`flex items-center
-              bg-[#0A66C2]  text-white hover:bg-[#16437E] focus:bg-[#16437E] active:bg-[#09223b] active:text-white/70 
-                `}
-                startIcon={ <PlusIcon className="w-3 h-3"/> }
-                onClick={() => router.push("/add-item")}
-                >
-                    Add Item
-                </Button>
-            </div>
-            {itemsExist() ? 
-                (
+            {itemsExist() ? ( 
+                <>
+                <div className="flex items-center justify-between mt-4 p-6 bg-white rounded-lg shadow-md">
+                    <Searchbar />
+                    <Button size="sm" variant="primary" 
+                    className={`flex items-center
+                    bg-[#0A66C2]  text-white hover:bg-[#16437E] focus:bg-[#16437E] active:bg-[#09223b] active:text-white/70 
+                    `}
+                    startIcon={ <PlusIcon className="w-3 h-3"/> }
+                    onClick={() => router.push("/add-item")}
+                    >
+                        Add Item
+                    </Button>
+                </div> 
                 <ItemTable tableItem={items}/>
-            ) : (
+                </>
+            ): (
                 <CreateItems/>
             )}
         </div>
