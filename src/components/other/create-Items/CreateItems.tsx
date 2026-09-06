c312026c23a95e88efcf324127c3253c16905462
ahmed <kenan.begic.2002@gmail.com>
2026-09-06 20:32:39 +0200

    fix(css): center buttons using flex container

--- src/components/other/create-Items/CreateItems.tsx
+++ src/components/other/create-Items/CreateItems.tsx
@@ -5,8 +5,9 @@ import { MultipleTshirt, Tshirt } from "@/icons";
 export default function CreateItems() {
 
     return(
-        <ComponentsCard className="flex-1 min-h-0 flex flex-col items-center justify-center" hidden>
-            <Button className="w-70 h-70 shrink-0 p-7 m-5 flex flex-col items-center justify-center">
+        <ComponentsCard className=" flex-1 flex flex-col h-full min-h-0" >
+            <div className="flex flex-row items-center justify-center h-full gap-5 ">
+            <Button className="w-70 h-70 shrink-0 p-7 flex flex-col items-center justify-center">
                 <Tshirt className="w-40 h-40"/>
                 <span className="text-center">
                     Create a multiple variants of 
@@ -14,13 +15,14 @@ export default function CreateItems() {
                     size/color of a shirt).
                 </span>
             </Button>
-            <Button className="w-70 h-70 shrink-0 p-7 m-5 flex flex-col items-center justify-center">
+            <Button className="w-70 h-70 shrink-0 p-7 flex flex-col items-center justify-center">
                 <Tshirt className="w-40 h-40"/>
                 <span className="text-center">
                     Create a standalone item that
                     you sell
                 </span>
             </Button>
+            </div>
         </ComponentsCard>
     )
 }
