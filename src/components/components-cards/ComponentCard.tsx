158259d025fadf205bf0eedadcf6083bc8391f25
ahmed <kenan.begic.2002@gmail.com>
2026-09-06 20:21:23 +0200

    feat: switch to flex container
    Changed the child component to use flex container.
    Removed the "hidden" value, as its unnecessary if
    no tittle is provided.
    Removed <div> wrapper form child component to avoid
    issues in centering component.

--- src/components/components-cards/ComponentCard.tsx
+++ src/components/components-cards/ComponentCard.tsx
@@ -7,23 +7,20 @@ interface ComponentsCardProps {
 }
 
 const ComponentsCard: React.FC<ComponentsCardProps> = ({
-    title,
+    title = "",
     children,
     className = "",
-    hidden = false,
 }) => {
     return (
         <div className={`rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/3
         ${className}`}
         >
-            <div className={`px-6 py-5 ${hidden ? "hidden" : ""}`}>
+            <div className={`px-6 py-5 border-b border-gray-100 dark:border-gray-800 ${title === "" ? "hidden" : ""}`}>
                 <h3 className="text-base font-bold text-gray-800 dark:text-white/90">
                     {title}
                 </h3>
             </div>
-            <div className="p-4 border-t border-gray-100 dark:border-gray-800 sm:p-6">
-                <div className="space-y-6">{children}</div>
-            </div>
+            <div className="space-y-6 p-4 flex-1 grid sm:p-6">{children}</div>
         </div>
     )
 };
